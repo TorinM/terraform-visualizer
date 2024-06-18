9f94e44420d7bcd3df7fda14cd68748caadbad46
TorinM <torinmay@gmail.com>
2024-06-18 13:36:40 -0600

    Cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use warp::Filter;
-
 use std::sync::Arc;
 
 mod input;
@@ -7,8 +6,7 @@ mod parser;
 
 #[tokio::main]
 async fn main() {
-    let file_path = match input::get_file_path()
-    {
+    let file_path = match input::get_file_path() {
         Ok(path) => Arc::new(path),
         Err(e) => {
             eprintln!("{}", e);
@@ -18,26 +16,21 @@ async fn main() {
 
     let static_files = warp::fs::dir("static");
 
-    let data = 
-    {
-        let file_path = file_path.clone();
+    let data = {
+        let file_path = Arc::clone(&file_path);
         warp::path("data")
             .map(move || {
-                let file_path = Arc::clone(&file_path);
                 match input::read_input(&file_path) {
-                    Ok(json_data_raw) => {
-                        match parser::terraform::parse_terraform(&json_data_raw)
-                        {
-                            Ok(data) => warp::reply::json(&data),
-                            Err(e) => {
-                                eprintln!("Fatal error parsing input. Message: {}", e);
-                                warp::reply::json(&serde_json::json!({"error": "Error parsing input file"}))
-                            }
+                    Ok(json_data_raw) => match parser::terraform::parse_terraform(&json_data_raw) {
+                        Ok(data) => warp::reply::json(&data),
+                        Err(e) => {
+                            eprintln!("Fatal error parsing input. Message: {}", e);
+                            warp::reply::json(&serde_json::json!({"error": "Error parsing input file"}))
                         }
                     },
                     Err(e) => {
                         eprintln!("Fatal error reading input. Message: {}", e);
-                        return warp::reply::json(&serde_json::json!({"error": "Error reading input file"}));
+                        warp::reply::json(&serde_json::json!({"error": "Error reading input file"}))
                     }
                 }
             })
@@ -47,5 +40,5 @@ async fn main() {
 
     // Start the server
     println!("Server started at http://127.0.0.1:3030");
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await
+    warp::serve(routes).run(([127,0,0,1], 3030)).await
 }
