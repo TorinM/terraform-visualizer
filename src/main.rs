4cbeb3cb478b08f76e4efbc19047448ea6568187
TorinM <torinmay@gmail.com>
2024-06-18 13:34:53 -0600

    Now can handle json file changing while server is up

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,48 @@
 use warp::Filter;
 
+use std::sync::Arc;
+
 mod input;
 mod parser;
 
 #[tokio::main]
 async fn main() {
-    // Serve static files from the "static" directory
+    let file_path = match input::get_file_path()
+    {
+        Ok(path) => Arc::new(path),
+        Err(e) => {
+            eprintln!("{}", e);
+            return;
+        }
+    };
+
     let static_files = warp::fs::dir("static");
 
-    let file_path = match input::get_file_path(){
-        Ok(path) => path,
-        Err(e) => {
-            eprintln!("Fatal getting file information. Message: {}", e);
-            return;
-        }
-    };
-
-    if let Err(e) = input::verify_file_exists(&file_path) {
-        eprintln!("Fatal error verifying input. Message: {}", e);
-        return;
-    }
-
-    let json_data_raw = match input::read_input(&file_path){
-        Ok(json_data) => json_data,
-        Err(e) => {
-            eprintln!("Fatal error reading input. Message: {}", e);
-            return;
-        }
-    };
-    let json_data = match parser::terraform::parse_terraform(&json_data_raw)
+    let data = 
     {
-        Ok(json_data) => json_data,
-        Err(e) => {
-            eprintln!("Fatal error parsing Terraform data. Verify input file is from `terraform show -json example.json`. Message: {}", e);
-            return;
-        }
+        let file_path = file_path.clone();
+        warp::path("data")
+            .map(move || {
+                let file_path = Arc::clone(&file_path);
+                match input::read_input(&file_path) {
+                    Ok(json_data_raw) => {
+                        match parser::terraform::parse_terraform(&json_data_raw)
+                        {
+                            Ok(data) => warp::reply::json(&data),
+                            Err(e) => {
+                                eprintln!("Fatal error parsing input. Message: {}", e);
+                                warp::reply::json(&serde_json::json!({"error": "Error parsing input file"}))
+                            }
+                        }
+                    },
+                    Err(e) => {
+                        eprintln!("Fatal error reading input. Message: {}", e);
+                        return warp::reply::json(&serde_json::json!({"error": "Error reading input file"}));
+                    }
+                }
+            })
     };
 
-    let json_data_clone = json_data.clone();
-    // Create a warp filter to handle the data endpoint
-    let data = warp::path("data")
-        .map(move || {
-            warp::reply::json(&json_data_clone)
-        });
-
-    // Combine filters
     let routes = static_files.or(data);
 
     // Start the server
